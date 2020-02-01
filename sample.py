732504ebbd8266d08741cbbf219c0bdbd5a243b3
cnprogrammer2019 <cnprogrammer@126.com>
2020-02-01 17:47:09 +0800

    add some samples after add noise on image

--- sample.py
+++ sample.py
@@ -18,16 +18,17 @@ from PIL import Image
 
 image_file_path = r'image/s.jpeg'
 old_image = Image.open(image_file_path)
+saved_image_quality = 10
 
 # 加线
 new_image = ii.add_noise_lines_to_image(old_image, noise_dense=0.5, start_color={'r': 0, 'g': 0, 'b': 0})
 new_image.show()
-new_image.save('s0.jpeg')
+new_image.save('s0.jpeg', quality=saved_image_quality)
 
 # 加随机噪点1
 new_image = ii.add_noise_points_to_image(old_image, noise_dense=0.15)
 new_image.show()
-new_image.save('s1.jpeg')
+new_image.save('s1.jpeg', quality=saved_image_quality)
 
 # 将指定颜色变换为指定颜色，效果不明显
 # new_image = imgf.reset_color(old_image, source_color=(0, 0, 0), destension_color=(255, 255, 255, 255))
@@ -39,7 +40,7 @@ new_image.save('s1.jpeg')
 
 # 固定噪点
 new_image = ii.add_random_points_to_image(old_image, noise_dense=0.3)
-new_image.save('s2.jpeg')
+new_image.save('s2.jpeg', quality=saved_image_quality)
 noise_points = []
 for h in range(960):
     for v in range(540):
@@ -47,4 +48,4 @@ for h in range(960):
 
 new_image = ii.add_points_to_image(new_image, noise_points, color={'r': 127, 'b': 127, 'g': 127})
 new_image.show()
-new_image.save('s3.jpeg')
+new_image.save('s3.jpeg', quality=saved_image_quality)
